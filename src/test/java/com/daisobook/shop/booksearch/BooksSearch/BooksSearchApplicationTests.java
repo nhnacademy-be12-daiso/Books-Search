9de32a6de86f1d12d3f3a587c35915839c93fd4b
hetgwi01 <hetgwi03@gmail.com>
2025-11-12 17:13:01 +0900

    init

---
+++ src/test/java/com/daisobook/shop/booksearch/BooksSearch/BooksSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.daisobook.shop.booksearch.BooksSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
