085d31f78e317081ac5d092a267bd3cc3e6cfa72
hetgwi01 <hetgwi03@gmail.com>
2025-12-17 19:53:26 +0900

    도서 세부도 할인 수정

--- src/main/java/com/daisobook/shop/booksearch/BooksSearch/service/book/impl/BookServiceImpl.java
+++ src/main/java/com/daisobook/shop/booksearch/BooksSearch/service/book/impl/BookServiceImpl.java
@@ -391,7 +391,7 @@ public class BookServiceImpl implements BookService {
                                         ba.getRole() != null ? ba.getRole().getId() : null,
                                         ba.getRole() != null ? ba.getRole().getName() : null))
                         .toList(),
-                book.getPublisher().getName(), book.getPublicationDate(), book.getPrice(), Objects.requireNonNull(i).setScale(2, RoundingMode.DOWN),
+                book.getPublisher().getName(), book.getPublicationDate(), book.getPrice(), i != null ? i.setScale(2, RoundingMode.DOWN) : null,
                 discountPrice, book.isPackaging(), book.getStock(), book.getStatus(), imageRespDTOS, book.getVolumeNo(), categoryRespDTOS,
                 tagRespDTOS, count, check, reviews);
     }
