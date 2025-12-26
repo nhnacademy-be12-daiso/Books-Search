73bbaeeeb9fa256ee447a51cabc7f73dd4033ae9
조선대-NHN 29 <chosun-nhn29@joseondae-NHNui-noteubug-2.local>
2025-12-26 09:20:16 +0900

    fix: search 기능 제외

--- src/main/java/com/daisobook/shop/booksearch/BooksSearch/search/exception/EmbeddingException.java
+++
@@ -1,5 +0,0 @@
-package com.daisobook.shop.booksearch.BooksSearch.search.exception;
-
-public class EmbeddingException extends SearchModuleException {
-    public EmbeddingException(String message, Throwable cause) { super("EmbeddingClient", message, cause); }
-}
