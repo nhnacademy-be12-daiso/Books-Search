222b0daf70ccbfbb18f3c585571a571721d4fa33
hetgwi01 <hetgwi03@gmail.com>
2026-01-05 13:20:32 +0900

    소나큐브 이슈 관련 로직 수정

--- src/main/java/com/daisobook/shop/booksearch/books_search/exception/custom/DuplicateResourceException.java
+++ src/main/java/com/daisobook/shop/booksearch/books_search/exception/custom/DuplicateResourceException.java
@@ -1,10 +1,8 @@
 package com.daisobook.shop.booksearch.books_search.exception.custom;
 
-import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
 // 409 Conflict 계열의 부모 (중복 데이터 등)
-@Getter
 public class DuplicateResourceException extends RuntimeException {
     private static final HttpStatus HTTP_STATUS = HttpStatus.CONFLICT;
     public DuplicateResourceException(String message) {
